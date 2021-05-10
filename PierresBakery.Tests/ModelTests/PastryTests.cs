ab1ec90ed93d0cff24c373371e520cc967f125bd
basicjohn <edmondsonj@gmail.com>
2021-05-09 17:04:50 -0700

    add 4 pastry class tests

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -1,27 +1,44 @@
-﻿// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using PierresBakery.Bread;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// namespace Bread.Tests
-// {
-//   [TestClass]
-//   public class BreadTests
-//   {
-//     [TestMethod]
-//     public void AddPastry_IsPastry_String()
-//     {
-//       Pastry testPastry = new Pastry("Croissant");
-//       Assert.AreEqual("Croissant", testBread.Type);
-//     }
+namespace PierresBakery.Pastries.Tests
+{
+  [TestClass]
+  public class TestingPastry
+  {
+    [TestMethod]
+    public void AddPastry_IsPastry_String()
+    {
+      Pastry testPastry = new Pastry("Como");
+      Assert.AreEqual("Como", testPastry.PastryType);
+    }
 
-//     [TestMethod]
-//     public void AddPastry_IsPrice_String()
-//     {
-//       Pastry testPastry = new Pastry("Croissant");
-//       Int testPrice = 2;
-//       Assert.AreEqual(testPrice, testPastry.Price);
-//     }
-//   }
-// }
+    [TestMethod]
+    public void AddPastry_IsPrice_String()
+    {
+      Pastry testPastry = new Pastry("Como");
+      int testPrice = 5;
+      Assert.AreEqual(testPrice, testPastry.Price);
+    }
 
 
 
+    [TestMethod]
+    public void AddPastry_IsQuantity_Int()
+    {
+      Pastry testPastry = new Pastry("Como");
+      int testQuantity = 5;
+      Assert.AreEqual(testQuantity, testPastry.AddPastry(5));
+    }
+
+
+    [TestMethod]
+    public void PastrySubtotal_IsSubtotal_Int()
+    {
+      Pastry testPastry = new Pastry("Como");
+      testPastry.AddPastry(41);
+      testPastry.PastrySubtotal();
+      int testSubtotal = 140;
+      Assert.AreEqual(testSubtotal, testPastry.PastrysSubtotal);
+    }
+  }
+}
